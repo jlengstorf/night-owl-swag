1a9e1f1a34b162c3d93f0d50ffe7088d69c98fb6
Jason Lengstorf <jason@lengstorf.com>
2020-07-29 22:35:23 -0700

    wip: getting closer

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -1,6 +1,57 @@
-exports.handler = async () => {
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async ({ body }) => {
+  const { price, quantity, size, description } = JSON.parse(body);
+  const validatedQuantity = quantity > 0 ? quantity : 1;
+
+  const session = await stripe.checkout.sessions.create({
+    mode: 'payment',
+    payment_method_types: ['card'],
+    billing_address_collection: 'auto',
+    shipping_address_collection: {
+      // Unsupported country codes:
+      // AS, CX, CC, CU, HM, IR, KP, MH, FM, NF, MP, PW, SD, SY, UM, VI
+      //
+      // if you don’t see your country listed, please add it as long as it’s
+      // not in the unsupported countries list
+      // (sorry, that’s Stripe policy, not mine)
+      // country codes: https://www.nationsonline.org/oneworld/country_code_list.htm
+      allowed_countries: [
+        'US',
+        'CA',
+        'MX',
+        'IE',
+        'GB',
+        'DE',
+        'JP',
+        'FR',
+        'HR',
+        'DK',
+        'NO',
+        'SE',
+        'FI',
+        'AU',
+      ],
+    },
+    line_items: [
+      {
+        price,
+        quantity: validatedQuantity,
+        description,
+      },
+    ],
+    metadata: {
+      size,
+    },
+    success_url: `${process.env.URL}/purchased`,
+    cancel_url: `${process.env.URL}/`,
+  });
+
   return {
     statusCode: 200,
-    body: 'boop',
+    body: JSON.stringify({
+      sessionId: session.id,
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    }),
   };
 };
