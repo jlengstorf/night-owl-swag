5d4de418f1cb79a0618fa09a3904aff7822a6391
Thorsten Schaeff <5748289+thorwebdev@users.noreply.github.com>
2020-08-11 04:21:05 +0800

    feat: add all supported shipping countries. (#5)

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -1,4 +1,5 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const shippingCountries = require('./data/shippingCountries.json');
 
 exports.handler = async ({ body }) => {
   const { price, quantity, size, description } = JSON.parse(body);
@@ -11,29 +12,8 @@ exports.handler = async ({ body }) => {
     shipping_address_collection: {
       // Unsupported country codes:
       // AS, CX, CC, CU, HM, IR, KP, MH, FM, NF, MP, PW, SD, SY, UM, VI
-      //
-      // if you don’t see your country listed, please add it as long as it’s
-      // not in the unsupported countries list
       // (sorry, that’s Stripe policy, not mine)
-      // country codes: https://www.nationsonline.org/oneworld/country_code_list.htm
-      allowed_countries: [
-        'US',
-        'CA',
-        'MX',
-        'IE',
-        'GB',
-        'DE',
-        'JP',
-        'FR',
-        'HR',
-        'DK',
-        'NO',
-        'SE',
-        'FI',
-        'AU',
-        'IN',
-        'AR',
-      ],
+      allowed_countries: shippingCountries,
     },
     line_items: [
       {
